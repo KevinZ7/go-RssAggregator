1093f5227c7ff6d20a2f2279d6fad57db3596f8d
unknown <kevin.z.zhou@gmail.com>
2024-02-07 03:00:08 -0800

    setup env variable and added godotenv dependency

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/kevinz7/go-RssAggregator
+
+go 1.21.6
+
+require github.com/joho/godotenv v1.5.1 // indirect
---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	fmt.Println("hello world")
+	godotenv.Load(".env")
+	portString := os.Getenv("port")
+	if portString == "" {
+		log.Fatal("PORT is not found in the environment")
+	}
+
+	fmt.Println("Port:", portString)
+}
