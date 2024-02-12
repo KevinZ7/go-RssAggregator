c9417d70f177c621c396a11a05884d83625fc0a0
unknown <kevin.z.zhou@gmail.com>
2024-02-11 19:10:39 -0800

    add auth file

--- handler_user.go
+++ handler_user.go
@@ -7,6 +7,7 @@ import (
 	"time"
 
 	"github.com/google/uuid"
+	"github.com/kevinz7/go-RssAggregator/internal/auth"
 	"github.com/kevinz7/go-RssAggregator/internal/database"
 )
 
@@ -41,6 +42,14 @@ func (apiCfg *apiConfig) handlerCreateUser(w http.ResponseWriter, r *http.Reques
 		return
 	}
 
-	respondWithJSON(w, 200, databaseUserToUser(user))
+	respondWithJSON(w, 201, databaseUserToUser(user))
 
+}
+
+func (apiCfg *apiConfig) handleGetUser(w http.ResponseWriter, r *http.Request) {
+	apiKey, err := auth.GetAPIKey(r.Header)
+	if err != nil {
+		respondWithError(w, 403, fmt.Sprintf("Auth error: %v", err))
+		return
+	}
 }
